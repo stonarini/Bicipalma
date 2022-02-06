ad7ea1aa2306c2136276bdad954116d488e46a5a
stonarini <stonarini@cifpfbmoll.eu>
2022-02-06 03:24:25 +0100

    feat(Bicicleta): add Bicicleta class, implementing the Movil interface

---
+++ src/main/java/edu/poniperro/domain/bicicleta/Bicicleta.java
@@ -0,0 +1,19 @@
+package edu.poniperro.domain.bicicleta;
+
+public class Bicicleta implements Movil {
+	private final int id;
+
+	public Bicicleta(int id) {
+		this.id = id;
+	}
+
+	@Override
+	public int getId() {
+		return this.id;
+	}
+
+	@Override
+	public String toString() {
+		return Integer.toString(this.id);
+	}
+}
